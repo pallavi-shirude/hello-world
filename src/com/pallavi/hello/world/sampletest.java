d33980bd23dd05cb14c8b37578246496899cf810
pallavi-shirude <shirude.pallavi@gmail.com>
2019-02-24 16:21:28 +1100

    created package

--- src/com/pallavi/hello/world/sampletest.java
+++ src/com/pallavi/hello/world/sampletest.java
@@ -1,9 +1,10 @@
+package com.pallavi.hello.world;
 
 public class sampletest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello pallaviiiiiiiiiiiiiiiiii");
+		System.out.println("hello pallaviiiiiiiiiiiiiiiiii \nHello world!");
 
 	}
 
